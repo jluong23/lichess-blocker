a28b3f10e9ec161a2e39aba0c0b3e830f0b6fe4b
James Luong <jamesluong@hotmail.co.uk>
2023-02-20 16:16:06 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  console.log("test");
+
+  return (
+    <div className="App">
+      <h1>Lichess blocker extension!</h1>
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default App;
